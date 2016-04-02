25aaf32659e8a10604623e930577de39f9524c67
Shaun Carland <shaun.carland@annkissam.com>
2016-04-02 18:24:09 -0400

    add 3/4 models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402191905) do
+ActiveRecord::Schema.define(version: 20160402221839) do
+
+  create_table "data_sources", force: :cascade do |t|
+    t.string   "source_name"
+    t.string   "source_description"
+    t.string   "source_publisher"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "location_name"
+    t.string   "location_type"
+    t.string   "country"
+    t.string   "state_province"
+    t.string   "district_county_municipality"
+    t.string   "city"
+    t.string   "alt_name1"
+    t.string   "alt_name2"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  create_table "zika_data_fields", force: :cascade do |t|
+    t.string   "data_field_code",        null: false
+    t.string   "data_field_name"
+    t.string   "data_field_description"
+    t.string   "data_field_unit"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "zikas", force: :cascade do |t|
     t.datetime "created_at", null: false
